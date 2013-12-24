6749d9e8767dfbdd4dfc3717494ac21fb5db8ea6
weiguili <gta-qic@gmail.com>
2013-12-24 17:36:54 +0800

    netty

--- src/io/netty/example/http/helloworld/HttpHelloWorldServerInitializer.java
+++ src/io/netty/example/http/helloworld/HttpHelloWorldServerInitializer.java
@@ -1,11 +1,20 @@
-package netty;
-
-/**
- * Desc:
- * User: weiguili(li5220008@gmail.com)
- * Date: 13-12-24
- * Time: 下午2:09
+/*
+ * Copyright 2013 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
  */
+package io.netty.example.http.helloworld;
+
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
@@ -23,6 +32,5 @@ public class HttpHelloWorldServerInitializer extends ChannelInitializer<SocketCh
 
         p.addLast("codec", new HttpServerCodec());
         p.addLast("handler", new HttpHelloWorldServerHandler());
-
     }
 }
